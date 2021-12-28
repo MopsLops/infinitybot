487bed74e956e89084816ad26ab4816db59e2203
MopsLops <azsssdaz400@gmail.com>
2021-12-28 03:17:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 const mineflayer = require('mineflayer')
-
-
-
 const bot = mineflayer.createBot({
     host: 'MeinKampf54.aternos.me',
     port: 36631,
     username: 'Bot',   
-  })
+  });
 
   bot.on('login', async () => {
     console.log('bot is on')
-  })
-
-  bot.once('spawn', () => {
-    bot.chat('Добро пожаловать в Казахстан!')
-  })
+  });
 
   bot.on('chat', (all_members_are_administrators, message) => {
     if (message == 'liv') {
       leave()
     }
-
-    if (message == 'Кто гей?')
-    bot.chat("/say [MetalDawin] > Я")
-  })
+  });
 
   function leave(){
     bot.end()
-  }
+  };
